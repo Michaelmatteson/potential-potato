eaedde04ab18929e5d92c576835920a0c2a232ba
Michaelmatteson <93003020+Michaelmatteson@users.noreply.github.com>
2022-05-20 09:40:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function draw() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+   save('myFilterImage.png')
+}
